a6b14440fefc458575381102751166770b98f372
fedamuhammadian <f.muhammadian@gmail.com>
2023-07-31 20:26:22 -0700

    url shortening part 2 has done

--- express_server.js
+++ express_server.js
@@ -1,28 +1,32 @@
-function generateRandomString(url) {
-    let randomString = '';
-    for (let i = 0; i < 6; i++) {
-      const randomIndex = Math.floor(Math.random() * url.length);
-      randomString += url.charAt(randomIndex);
-    }
-    return randomString;
-  };
-  
-const express = require("express");
+ const express = require("express");
 const app = express();
 const PORT = 8080; 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+    return randomString;
+};
+
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
-  app.post("/urls", (req, res) => {
-    console.log(req.body); 
-    res.send("Ok"); 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
   });
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
@@ -40,7 +44,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
